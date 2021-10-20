b12447c978235f20db45724f81bba1c19938f2e6
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-20 18:14:48 -0500

    src/data.js updated: project data added

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const projects = [
+    {
+        title: "Budget Tracker",
+        description:
+            "An application that allows users to add expenses and deposits to their budget with or without a connection.",
+        image: "./bt.png",
+        githubLink: "https://github.com/zavaletaosv/Budget-Tracker",
+        deployedLink: "https://budget-tracker-oz.herokuapp.com/"
+    },
+
+    {
+        title: "Catz-N-Dogz",
+        description:
+            "An application that allows users to post lost or found animals.",
+        image: "./cnd.png",
+        githubLink: "https://github.com/bboyd146/Catz-n-Dogz",
+        deployedLink: "https://catz-n-dogz1.herokuapp.com/"
+    },
+
+    {
+        title: "Music Spot",
+        description:
+            "A web application that uses data from multiple server-side API requests to allow users to search different music genres and have artists returned for that specific genre.",
+        image: "./ms.png",
+        githubLink: "https://github.com/sarah4coulson/1-music-spot",
+        deployedLink: "https://sarah4coulson.github.io/1-music-spot/"
+    },
+
+    {
+        title: "Work Day Scheduler",
+        description:
+            "An application allows the user to store events for each hour of a typical work day (9 am - 5 pm).",
+        image: "./planner.gif",
+        githubLink: "https://github.com/zavaletaosv/Work-Day-Scheduler",
+        deployedLink: "https://zavaletaosv.github.io/Work-Day-Scheduler/"
+    },
+
+    {
+        title: "Code Quiz",
+        description:
+            "A timed coding quiz with multiple-choice questions that deducts points from your score for incorrect answers.",
+        image: "./quiz.png",
+        githubLink: "https://github.com/zavaletaosv/Code-Quiz",
+        deployedLink: "https://zavaletaosv.github.io/Code-Quiz/"
+    },
+
+    {
+        title: "Team Profile Generator",
+        description:
+            "An application that allows the user to create a team directly from the terminal by inputting certain criteria. The terminal would then render an HTML page with the information the user input.",
+        image: "./team-gen.png",
+        githubLink: "https://github.com/zavaletaosv/Team-Profile-Generator/blob/main/README.md",
+        deployedLink: "https://drive.google.com/file/d/1q20fM6B73SQEEqXzzMmYyEj_YGPufUdl/view"
+    },
+];
+
