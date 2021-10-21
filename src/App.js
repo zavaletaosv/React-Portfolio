bbac75a3ab8a92a52e5b1a901c46bb490bc3496d
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-20 19:42:23 -0500

    src/App.js updated: imported components, created route paths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+    <main className="text-gray-400 bg-gray-900 body-font max-h-screen"> 
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+      <Footer />
+    </main>
+  </BrowserRouter>
+    );
 }
-
-export default App;
